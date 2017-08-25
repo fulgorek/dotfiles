7546f09326cf871fe7061e63730a00db0051d55a
Alejandro Torres <fulgorek@gmail.com>
2017-08-25 13:40:51 -0500

    stuff

--- brew.sh
+++ brew.sh
@@ -49,48 +49,58 @@ brew install sfnt2woff
 brew install sfnt2woff-zopfli
 brew install woff2
 
-# Install some CTF tools; see https://github.com/ctfs/write-ups.
-brew install aircrack-ng
-brew install bfg
-brew install binutils
-brew install binwalk
-brew install cifer
-brew install dex2jar
-brew install dns2tcp
-brew install fcrackzip
-brew install foremost
-brew install hashpump
-brew install hydra
-brew install john
-brew install knock
-brew install netpbm
-brew install nmap
-brew install pngcheck
-brew install socat
-brew install sqlmap
-brew install tcpflow
-brew install tcpreplay
-brew install tcptrace
-brew install ucspi-tcp # `tcpserver` etc.
-brew install xpdf
-brew install xz
-
 # Install other useful binaries.
 brew install ack
 #brew install exiv2
 brew install git
 brew install git-lfs
 brew install imagemagick --with-webp
-brew install lua
-brew install lynx
-brew install p7zip
-brew install pigz
+# brew install lua
+# brew install lynx
+# brew install p7zip
+# brew install pigz
 brew install pv
 brew install rename
 brew install ssh-copy-id
 brew install tree
-brew install vbindiff
-brew install zopfli
+# brew install vbindiff
+# brew install zopfli
+
+brew install ansible
+brew install aws-elasticbeanstalk
+brew install caddy
+brew install chtf
+brew install docker
+brew install docker-compose
+brew install docker-machine
+brew install elixir
+brew install erlang
+brew install ffmpeg
+brew install fish
+# brew install git
+brew install git-crypt
+# brew install glib
+brew install go
+brew install grep
+brew install htop
+brew install httpie
+brew install hugo
+brew install imagemagick
+brew install lame
+brew install mysql
+brew install ncdu
+brew install nmap
+brew install python
+brew install speedtest_cli
+brew install sqlite
+brew install tmate
+brew install tmux
+brew install tree
+brew install vim
+brew install wget
+brew install youtube-dl
+brew install zsh
+
 
 # Remove outdated versions from the cellar.
 brew cleanup
